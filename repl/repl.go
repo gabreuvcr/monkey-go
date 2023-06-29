b44d14341c6e28a812862870250e67df4fc8d167
Gabriel Rocha <gabreuvcr@gmail.com>
2023-06-29 00:51:58 -0300

    Some changes in repl

--- repl/repl.go
+++ repl/repl.go
@@ -3,18 +3,26 @@ package repl
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"monkey/lexer"
 	"monkey/token"
+	"os"
+	"os/user"
 )
 
-const PROMPT = ">> "
+func Start() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
 
-func Start(in io.Reader, out io.Writer) {
+	in, out := os.Stdin, os.Stdout
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Fprint(out, PROMPT)
+		fmt.Fprint(out, ">> ")
 		scanned := scanner.Scan()
 		if !scanned {
 			return
